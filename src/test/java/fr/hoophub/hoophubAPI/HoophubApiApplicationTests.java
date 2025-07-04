7c3d2a446ca9fc927631dcfcfe7529dc74770345
Ugo Bertrand <ugo.bertrand@epitech.eu>
2025-07-04 10:29:22 +0200

    initialize api with spring boot

---
+++ src/test/java/fr/hoophub/hoophubAPI/HoophubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hoophub.hoophubAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoophubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
