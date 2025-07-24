8f7868be8a5a224a5bb90f5bf1eecaac0bca89d7
Ugo Bertrand <ugo.bertrand@epitech.eu>
2025-07-24 13:30:54 +0200

    feat(authentication): add UnauthenticatedEntryPoint for the wrong authentication

---
+++ src/main/java/fr/hoophub/hoophubAPI/config/security/UnauthenticatedEntryPoint.java
@@ -0,0 +1,29 @@
+package fr.hoophub.hoophubAPI.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.hoophub.hoophubAPI.common.ExceptionResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class UnauthenticatedEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        if(!response.isCommitted()){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                    .code(HttpStatus.UNAUTHORIZED.value())
+                    .message("You are not authenticated to access this resource.")
+                    .status(HttpStatus.UNAUTHORIZED.toString())
+                    .build();
+            new ObjectMapper().writeValue(response.getOutputStream(), exceptionResponse);
+        }
+    }
+}
