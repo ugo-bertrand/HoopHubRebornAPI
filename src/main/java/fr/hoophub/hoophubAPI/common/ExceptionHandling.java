de7bdab1d8a68a7738995079d9a416a304498eb1
Ugo Bertrand <ugo.bertrand@epitech.eu>
2025-08-08 10:48:13 +0200

    feat(authentication): fix ExceptionHandling file to handle bad request

--- src/main/java/fr/hoophub/hoophubAPI/common/ExceptionHandling.java
+++ src/main/java/fr/hoophub/hoophubAPI/common/ExceptionHandling.java
@@ -1,6 +1,9 @@
 package fr.hoophub.hoophubAPI.common;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,7 +12,18 @@ public class ExceptionHandling {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ExceptionResponse> handleBaseException(BaseException e){
-        ExceptionResponse response = new ExceptionResponse(e.getMessage(),e.getHttpStatus().toString(),e.getHttpStatus().value());
+        ExceptionResponse response = new ExceptionResponse(e.getMessage(),e.getHttpStatus().name(),e.getHttpStatus().value());
         return new ResponseEntity<>(response, e.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        String message = e.getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("Validation of the field failed");
+        ExceptionResponse response = new ExceptionResponse(message, HttpStatus.BAD_REQUEST.name(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
